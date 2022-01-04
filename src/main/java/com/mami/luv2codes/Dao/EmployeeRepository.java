32ebbf1703937685ee6c0f8a190deb9c7792cc66
Alexander Mami <algru81@gmail.com>
2022-01-04 21:27:06 +0300

    finish lesson 77

--- src/main/java/com/mami/luv2codes/Dao/EmployeeRepository.java
+++ src/main/java/com/mami/luv2codes/Dao/EmployeeRepository.java
@@ -1,10 +1,10 @@
 package com.mami.luv2codes.Dao;
 
 import com.mami.luv2codes.model.Employee;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
 
